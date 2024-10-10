232aa8d16312c19148c7b6a3b68cb915bc3a2b95
Arthur Fornaciari Gaviorno <103372834+arthurgaviorno@users.noreply.github.com>
2024-10-10 14:32:31 -0300

    Add files via upload
    adicionando um arquivo x no repositorio

---
+++ aula6.py
@@ -0,0 +1,7 @@
+#conversao de tipos, coercao
+#é o ato de converter um tipo em outro 
+#tipos imutáveis e primitivos: str, int, float, bool
+
+print(int('1') + 1) #str -> int
+print('a' + str(2)) #int -> str
+
